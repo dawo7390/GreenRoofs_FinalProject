699e2963b15d067836e44d3f531320c7b226fe6e
dawo7390 <82596120+dawo7390@users.noreply.github.com>
2022-11-02 11:50:52 -0600

    map works!

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
-var map;
 
-//function to instantiate the Leaflet map
-function createMap(){
-    //create the map
-    map = L.map('map', {
-        center: [0, 0],
-        zoom: 1
-    });
+var map = L.map('map').setView([40.69, -73.97], 10);
 
-    //add OSM base tilelayer
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    }).addTo(map);
-};
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
