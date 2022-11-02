ce31dda8c83e93f5aa78a0fb02c6537f2952e5fc
dawo7390 <82596120+dawo7390@users.noreply.github.com>
2022-11-02 15:45:11 -0600

    more work

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 
-var map = L.map('map').setView([40.69, -73.97], 10);
+var mymap = L.map('mapID').setView([40.69, -73.97], 10);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+//add tile layer...replace project id and accessToken with your own
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{40.69}/{-73.97}/{y}.png?access_token={pk.eyJ1IjoiZGF3bzczOTAiLCJhIjoiY2w3cXIweXJjMDdpNzQxbzB5Zjdhajc4YiJ9.qPWM7J5wt_NNdCy-NxRuJw}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapID'
+}).addTo(mymap);
+
+
