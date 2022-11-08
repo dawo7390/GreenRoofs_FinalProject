ad844689187e5e3716163e8906f552485e9c7d56
dawo7390 <82596120+dawo7390@users.noreply.github.com>
2022-11-08 16:52:29 -0700

    pre-class 11/8

--- js/barChart.js
+++ js/barChart.js
@@ -1,1 +1,18 @@
 // D3 BARCHART 
+
+// Set graph margins and dimensions
+var margin = {top: 20, right: 500, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+var x = d3.scaleBand()
+          .range([0, width])
+          .padding(0.1);
+var y = d3.scaleLinear()
+          .range([height, 0]);
+var svg = d3.select("barChart").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", 
+                "translate(" + margin.left + "," + margin.top + ")")
+        
