762abd92d8b9b557b6d59f62eb2d91697ed400e8
dawo7390 <82596120+dawo7390@users.noreply.github.com>
2022-11-08 10:26:31 -0700

    boroughs highlighted

--- js/map.js
+++ js/map.js
@@ -1,135 +1,17 @@
-var width = 500,
-    
-    height = 600,
-    centered;
+//LEAFLET MAP
 
-// Define color scale
-var color = d3.scale.linear()
-  .domain([1, 20])
-  .clamp(true)
-  .range(['#fff', '#409A99']);
+let map = L.map('map').setView([40.69, -73.97], 10);
 
-var projection = d3.geo.mercator()
-  .scale(45000)
-  // Center the Map in Colombia
-  .center([-73.935242, 40.730610])
-  .translate([width / 2, height / 2]);
-
-var path = d3.geo.path()
-  .projection(projection);
-
-// Set svg width & height
-var svg = d3.select('svg')
-  .attr('width', width)
-  .attr('height', height);
-
-// Add background
-svg.append('rect')
-  .attr('class', 'background')
-  .attr('width', width)
-  .attr('height', height)
-  .on('click', clicked);
-
-var g = svg.append('g');
-
-var effectLayer = g.append('g')
-  .classed('effect-layer', true);
-
-var mapLayer = g.append('g')
-  .classed('map-layer', true);
-
-var dummyText = g.append('text')
-  .classed('dummy-text', true)
-  .attr('x', 10)
-  .attr('y', 30)
-  .style('opacity', 0);
-
-var bigText = g.append('text')
-  .classed('big-text', true)
-  .attr('x', 20)
-  .attr('y', 45);
-
-// Load map data
-d3.json('data/nyc.geojson', function(error, mapData) {
-  var features = mapData.features;
-
-  // Update color scale domain based on data
-  color.domain([0, d3.max(features, nameLength)]);
-
-  // Draw each province as a path
-  mapLayer.selectAll('path')
-      .data(features)
-    .enter().append('path')
-      .attr('d', path)
-      .attr('vector-effect', 'non-scaling-stroke')
-      .style('fill', fillFn)
-      .on('mouseover', mouseover)
-      .on('mouseout', mouseout)
-      .on('click', clicked);
+var NYCmap = L.tileLayer('https://api.mapbox.com/styles/v1/dawo7390/cl9zygbct000114ofk5m565pk/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGF3bzczOTAiLCJhIjoiY2w3cXIweXJjMDdpNzQxbzB5Zjdhajc4YiJ9.qPWM7J5wt_NNdCy-NxRuJw', {
+  maxZoom: 18,
+  attribution: '&copy; <a href=”https://www.mapbox.com/about/maps/”>Mapbox</a> &copy; <a href=”http://www.openstreetmap.org/copyright”>OpenStreetMap</a>'
 });
 
-// Get province name
-function nameFn(d){
-  return d && d.properties ? d.properties.boro_name : null;
-}
+NYCmap.addTo(map)
 
-// Get province name length
-function nameLength(d){
-  var n = nameFn(d);
-  return n ? n.length : 0;
-}
 
-// Get province color
-function fillFn(d){
-  return color(nameLength(d));
-}
-
-// When clicked, zoom in
-function clicked(d) {
-  var x, y, k;
-
-  // Compute centroid of the selected path
-  if (d && centered !== d) {
-    var centroid = path.centroid(d);
-    x = centroid[0];
-    y = centroid[1];
-    k = 4;
-    centered = d;
-  } else {
-    x = width / 2;
-    y = height / 2;
-    k = 1;
-    centered = null;
-  }
-
-  // Highlight the clicked province
-  mapLayer.selectAll('path')
-    .style('fill', function(d){return centered && d===centered ? '#D5708B' : fillFn(d);});
-
-  // Zoom
-  g.transition()
-    .duration(750)
-    .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')scale(' + k + ')translate(' + -x + ',' + -y + ')');
-}
-
-function mouseover(d){
-  // Highlight hovered province
-  d3.select(this).style('fill', 'orange');
-
-  // Draw effects
-  textArt(nameFn(d));
-}
-
-function mouseout(d){
-  // Reset province color
-  mapLayer.selectAll('path')
-    .style('fill', function(d){return centered && d===centered ? '#D5708B' : fillFn(d);});
-
-  // Remove effect text
-  effectLayer.selectAll('text').transition()
-    .style('opacity', 0)
-    .remove();
-
-  // Clear province name
-  bigText.text('');
-}
+   //load the data...Example 2.3 line 22
+   $.getJSON("data/nyc.geojson", function(response){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(response).addTo(map);
+});
