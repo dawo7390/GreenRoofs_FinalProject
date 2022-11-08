d2b2736123614e253f774bbb3d7d64455d70bc77
dawo7390 <82596120+dawo7390@users.noreply.github.com>
2022-11-08 10:38:58 -0700

    site popups

--- js/map.js
+++ js/map.js
@@ -1,17 +1,26 @@
 //LEAFLET MAP
 
+//Create BaseMap
 let map = L.map('map').setView([40.69, -73.97], 10);
-
 var NYCmap = L.tileLayer('https://api.mapbox.com/styles/v1/dawo7390/cl9zygbct000114ofk5m565pk/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGF3bzczOTAiLCJhIjoiY2w3cXIweXJjMDdpNzQxbzB5Zjdhajc4YiJ9.qPWM7J5wt_NNdCy-NxRuJw', {
   maxZoom: 18,
   attribution: '&copy; <a href=”https://www.mapbox.com/about/maps/”>Mapbox</a> &copy; <a href=”http://www.openstreetmap.org/copyright”>OpenStreetMap</a>'
 });
-
+//Highlight Boroughs
 NYCmap.addTo(map)
-
-
-   //load the data...Example 2.3 line 22
-   $.getJSON("data/nyc.geojson", function(response){
-    //create a Leaflet GeoJSON layer and add it to the map
+$.getJSON("data/nyc.geojson", function(response){ //create a Leaflet GeoJSON layer and add it to the map
     L.geoJson(response).addTo(map);
 });
+
+//Create GreenRoof Popup markers
+$.get('data/GreenRoofData2016_20180917.csv', function(csvString) {
+    var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;  // Use PapaParse to convert string to array of objects
+    for (var i in data) {
+      var row = data[i];
+      var marker = L.marker([row.ycoord, row.xcoord], {
+        opacity: 1
+      }).bindPopup(row.address);
+      marker.addTo(map);
+    }
+
+  });
