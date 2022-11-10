e1b187f82e374d5913e065993d3fabedd08c14be
dawo7390 <82596120+dawo7390@users.noreply.github.com>
2022-11-10 14:45:48 -0700

    clustering working

--- js/map.js
+++ js/map.js
@@ -1,37 +1,46 @@
-//LEAFLET MAP
+// //LEAFLET MAP
+"use strict";
+var mapFunction = {}; //declares fucntion holder
 
-//Create BaseMap
-let map = L.map('map').setView([40.69, -73.97], 10);
-var NYCmap = L.tileLayer('https://api.mapbox.com/styles/v1/dawo7390/cl9zygbct000114ofk5m565pk/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGF3bzczOTAiLCJhIjoiY2w3cXIweXJjMDdpNzQxbzB5Zjdhajc4YiJ9.qPWM7J5wt_NNdCy-NxRuJw', {
-  maxZoom: 18,
-  attribution: '&copy; <a href=”https://www.mapbox.com/about/maps/”>Mapbox</a> &copy; <a href=”http://www.openstreetmap.org/copyright”>OpenStreetMap</a>'
-});
+//declaring function 1
+mapFunction.addPopups = function (feature, layer) {
+  if (feature.properties && feature.properties.address) {
+    layer.bindPopup("<b>Address:</b>" + feature.properties.address);
+  }
+}
 
-//Highlight Boroughs
-NYCmap.addTo(map)
-$.getJSON("data/nyc.geojson", function(response){ //create a Leaflet GeoJSON layer and add it to the map
-    L.geoJson(response).addTo(map);
-});
+//declaring function 2
+mapFunction.pointToCircle =function (feature, latlng) {
+  var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "green",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+  };
+  var circleMarker = L.circleMarker(latlng, geojsonMarkerOptions);
+  return circleMarker;
+}
+window.onload=function(){
+//execute
+    let map = L.map('map').setView([40.69, -73.97], 10);
+    var NYCmap = L.tileLayer('https://api.mapbox.com/styles/v1/dawo7390/cl9zygbct000114ofk5m565pk/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGF3bzczOTAiLCJhIjoiY2w3cXIweXJjMDdpNzQxbzB5Zjdhajc4YiJ9.qPWM7J5wt_NNdCy-NxRuJw', {
+    maxZoom: 18,
+    attribution: '&copy; <a href=”https://www.mapbox.com/about/maps/”>Mapbox</a> &copy; <a href=”http://www.openstreetmap.org/copyright”>OpenStreetMap</a>'
+    }).addTo(map);
+    $.getJSON("data/nyc.geojson", function(response){ //create a Leaflet GeoJSON layer and add it to the map
+        L.geoJson(response).addTo(map);
+    });
+    // bikeThefts is the variable name we difined in Bike_Thefts_2011.js file. 
+    var greenLayerGroup = L.geoJSON(mygeojson, {
+    onEachFeature: mapFunction.addPopups,
+    pointToLayer: mapFunction.pointToCircle
+    });
 
-//Create GreenRoof Popup markers
-$.get('data/GreenRoofData2016_20180917.csv', function(csvString) {
-    var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;  // Use PapaParse to convert string to array of objects
-    for (var i in data) {
-      var row = data[i];
-      var mystring = "Address: "+row.address + '\r\n'+ "Zone: "+ row.zonedist1 +'\r\n' +"Owner Type: "+row.ownertype+ '\r\n';
-      var marker = L.marker([row.ycoord, row.xcoord], {
-        opacity: 1,
-        icon: L.icon({
-            iconUrl: 'img/images/marker-icon.png',
-            iconSize: [25, 38]})
-      }).bindPopup(mystring);
-      marker.addTo(map);
-    }
-});
-var clusters = L.markerClusterGroup();  //CLUSTERS ARE IN LAB 3
-clusters.addLayer(data);
-map.addLayer(clusters);
-
-
-
-   // address + zone + socioeconomic status
+    var clusters = L.markerClusterGroup();
+    clusters.addLayer(greenLayerGroup);
+    map.addLayer(clusters);
+    //mapObject.addLayer(bikesLayerGroup);
+    map.fitBounds(greenLayerGroup.getBounds());
+}
